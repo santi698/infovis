3f16a2325e50f3459475509327c187d4465994ae
Santiago Ocamica <santi6982@gmail.com>
2016-03-17 10:11:33 -0300

    Added word cloud to index

---
+++ 1.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+import inflection
+import re
+
+file = open('infovis-definitions.txt', 'r')
+data = file.read()
+# Replace all non alphabetic or spaces with spaces
+data = re.sub(r'[^\w ]', ' ', data)
+# Replace multiple spaces with just 1 space
+data = re.sub(r'\s\s+', ' ', data)
+
+words = list(filter(lambda d: len(d) > 3, map(lambda word: inflection.singularize(word), data.lower().split())))
+
+d = defaultdict(int)
+
+for word in words:
+    d[word] += 1
+
+print(list(filter(lambda item: item[1] > 2, map(lambda item: list(item), d.items()))))
